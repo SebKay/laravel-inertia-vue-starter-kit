2d516a9f58df6e977f4fc2a62b2cbf8440d846ef
Seb Kay <seb@sebkay.com>
2025-10-31 07:38:55 +0000

    Remove `pxlrbt/filament-environment-indicator`

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,15 +3,12 @@
 namespace App\Providers\Filament;
 
 use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
-use App\Enums\Environment;
-use App\Enums\Role;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -19,7 +16,6 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use pxlrbt\FilamentEnvironmentIndicator\EnvironmentIndicatorPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -41,15 +37,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->plugins([
                 FilamentSpatieRolesPermissionsPlugin::make(),
-                EnvironmentIndicatorPlugin::make()
-                    ->visible(fn () => \auth()->user()?->hasRole(Role::SUPER_ADMIN) && \app()->environment() !== Environment::PRODUCTION->value)
-                    ->showBorder(false)
-                    ->color(fn () => match (app()->environment()) {
-                        Environment::PRODUCTION->value => Color::Green,
-                        Environment::STAGING->value => Color::Blue,
-                        Environment::LOCAL->value => Color::Red,
-                        default => Color::Gray,
-                    }),
             ])
             ->middleware([
                 EncryptCookies::class,
