bba26dced3854a735eb6be85e511cf5704626c5c
Seb Kay <seb@sebkay.com>
2026-01-19 05:19:27 +0000

    Add password defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
 use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
@@ -31,6 +32,14 @@ class AppServiceProvider extends ServiceProvider
         JsonResource::withoutWrapping();
 
         Vite::useAggressivePrefetching();
+
+        Password::defaults(function () {
+            $rule = Password::min(6);
+
+            return $this->app->environment([Environment::PRODUCTION->value, Environment::STAGING->value])
+                ? $rule->mixedCase()->numbers()->symbols()->uncompromised()
+                : $rule;
+        });
 
         Table::configureUsing(function (Table $table): void {
             $table
