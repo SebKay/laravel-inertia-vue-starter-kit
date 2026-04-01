e50ef0c179414a20a78d5772f55030f09e68dcd0
Seb Kay <seb@sebkay.com>
2026-04-01 17:31:08 +0100

    Use attributes on `User` model

--- tests/Integration/UserTest.php
+++ tests/Integration/UserTest.php
@@ -2,35 +2,6 @@
 
 use App\Models\User;
 use Database\Seeders\RolesAndPermissionsSeeder;
-use Illuminate\Support\Facades\Hash;
-
-it("can update it's password", function () {
-    $user = User::factory()->create([
-        'password' => Hash::make('oldPassword#123'),
-    ]);
-
-    expect(Hash::check('oldPassword#123', $user->password))->toBeTrue();
-
-    $user->updatePassword('newPassword#123');
-
-    expect(Hash::check('newPassword#123', $user->refresh()->password))->toBeTrue();
-});
-
-it("doesn't update it's password if the value is empty", function () {
-    $user = User::factory()->create([
-        'password' => Hash::make('oldPassword#123'),
-    ]);
-
-    expect(Hash::check('oldPassword#123', $user->password))->toBeTrue();
-
-    $user->updatePassword('');
-
-    expect(Hash::check('oldPassword#123', $user->refresh()->password))->toBeTrue();
-
-    $user->updatePassword(null);
-
-    expect(Hash::check('oldPassword#123', $user->refresh()->password))->toBeTrue();
-});
 
 describe('With Roles and Permissions', function () {
     beforeEach(function () {
