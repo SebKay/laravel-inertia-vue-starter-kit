ffde7fd6995b5e063f43c35ed66c7f175d571f0f
Seb Kay <seb@sebkay.com>
2026-04-01 21:13:23 +0100

    Move page heading to component

---
+++ resources/js/Components/PageHeading.vue
@@ -0,0 +1,14 @@
+<template>
+    <div v-if="heading" class="mb-4 xl:mb-8">
+        <h1
+            class="text-3xl font-medium text-neutral-900 xl:text-4xl"
+            v-text="heading"
+        ></h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        heading?: string;
+    }>();
+</script>
