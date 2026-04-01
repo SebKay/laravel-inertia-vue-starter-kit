3337d740e37523b6d7911947967a696fcbab413c
Seb Kay <seb@fhoke.com>
2026-04-01 09:55:53 +0100

    Update Rector config

--- rector.php
+++ rector.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use RectorLaravel\Rector\FuncCall\RemoveDumpDataDeadCodeRector;
-use RectorLaravel\Rector\MethodCall\ChangeQueryWhereDateValueWithCarbonRector;
-use RectorLaravel\Set\LaravelLevelSetList;
-use RectorLaravel\Set\LaravelSetList;
+use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -15,25 +13,18 @@ return RectorConfig::configure()
         __DIR__.'/bootstrap/app.php',
         __DIR__.'/bootstrap/providers.php',
         __DIR__.'/config',
-        __DIR__.'/lang',
         __DIR__.'/database',
+        __DIR__.'/lang',
+        __DIR__.'/public/index.php',
         __DIR__.'/routes',
         __DIR__.'/tests',
     ])
-    ->withPhpSets(php84: true)->withSets([
-        LaravelLevelSetList::UP_TO_LARAVEL_120,
-        LaravelSetList::LARAVEL_CODE_QUALITY,
-        LaravelSetList::LARAVEL_COLLECTION,
-        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
-        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
-        LaravelSetList::LARAVEL_IF_HELPERS,
-        LaravelSetList::LARAVEL_TESTING,
-        LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
-    ])
+    ->withPhpSets(php84: true)
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withComposerBased(laravel: true)
     ->withConfiguredRule(RemoveDumpDataDeadCodeRector::class, [
         'dd', 'dump', 'var_dump', 'ray',
     ])
     ->withSkip([
         AddOverrideAttributeToOverriddenMethodsRector::class,
-        ChangeQueryWhereDateValueWithCarbonRector::class,
     ]);
