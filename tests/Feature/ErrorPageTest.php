cb93fa7ba8529ce52e86a1c816d71348797de0a1
Seb Kay <seb@sebkay.com>
2026-04-01 18:15:42 +0100

    Add some bit to app service provider

--- tests/Feature/ErrorPageTest.php
+++ tests/Feature/ErrorPageTest.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Enums\Environment;
 use Inertia\Testing\AssertableInertia as Assert;
 
 use function Pest\Laravel\get;
 
-test('missing pages render the inertia error page outside testing', function () {
-    app()->instance('env', Environment::LOCAL->value);
-
+test('missing pages render the inertia error page', function () {
     get('/this-page-does-not-exist')
         ->assertNotFound()
         ->assertInertia(
