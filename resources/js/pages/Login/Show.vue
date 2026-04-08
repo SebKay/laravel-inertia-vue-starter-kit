7ee833580764f4aab324d7524c549eeeeea3a926
Seb Kay <seb@sebkay.com>
2026-04-08 21:26:13 +0100

    Update Show.vue

--- resources/js/pages/Login/Show.vue
+++ resources/js/pages/Login/Show.vue
@@ -103,11 +103,7 @@
     import type { PageProps } from "@js/types/inertia";
 
     import { Button } from "@/components/ui/button";
-    import {
-        Card,
-        CardContent,
-        CardFooter,
-    } from "@/components/ui/card";
+    import { Card, CardContent, CardFooter } from "@/components/ui/card";
     import { Checkbox } from "@/components/ui/checkbox";
     import { Input } from "@/components/ui/input";
     import { Label } from "@/components/ui/label";
