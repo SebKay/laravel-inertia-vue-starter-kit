75529e5d025007957b72498b9283f91cfee6001d
Seb Kay <seb@fhoke.com>
2026-04-09 11:47:23 +0100

    Fix double-hashing in UserFactory, add return types, and remove unused param

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 /**
@@ -13,54 +13,59 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    public function definition()
+    protected static string $password = 'password';
+
+    /**
+     * @return array{name: string, email: string, email_verified_at: Carbon, password: string, remember_token: string}
+     */
+    public function definition(): array
     {
         return [
             'name' => \fake()->name(),
             'email' => \fake()->unique()->safeEmail(),
             'email_verified_at' => \now(),
-            'password' => Hash::make(config('seed.users.super.password')),
+            'password' => static::$password,
             'remember_token' => Str::random(10),
         ];
     }
 
-    public function unverified()
+    public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
 
-    public function super(?string $email = null)
+    public function super(): static
     {
         return $this
             ->state(fn (array $attributes) => [
                 'email' => config('seed.users.super.email'),
-                'password' => Hash::make(config('seed.users.super.password')),
+                'password' => config('seed.users.super.password'),
             ])
             ->afterCreating(function (User $user) {
                 $user->assignRole(Role::SUPER);
             });
     }
 
-    public function admin()
+    public function admin(): static
     {
         return $this
             ->state(fn (array $attributes) => [
                 'email' => config('seed.users.admin.email'),
-                'password' => Hash::make(config('seed.users.admin.password')),
+                'password' => config('seed.users.admin.password'),
             ])
             ->afterCreating(function (User $user) {
                 $user->assignRole(Role::ADMIN);
             });
     }
 
-    public function user()
+    public function user(): static
     {
         return $this
             ->state(fn (array $attributes) => [
                 'email' => config('seed.users.user.email'),
-                'password' => Hash::make(config('seed.users.user.password')),
+                'password' => config('seed.users.user.password'),
             ])
             ->afterCreating(function (User $user) {
                 $user->assignRole(Role::USER);
