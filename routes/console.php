1313cb7afa3a32ee0bf890c64dd82ed176e46302
Seb Kay <seb@fhoke.com>
2026-04-09 11:37:25 +0100

    Add withoutOverlapping() to health check scheduler

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use App\Enums\Environment;
 use Illuminate\Support\Facades\Schedule;
 use Spatie\Health\Commands\RunHealthChecksCommand;
 
-Schedule::command(RunHealthChecksCommand::class)->everyMinute()->environments(Environment::PRODUCTION->value);
+Schedule::command(RunHealthChecksCommand::class)->everyMinute()->withoutOverlapping()->environments(Environment::PRODUCTION->value);
