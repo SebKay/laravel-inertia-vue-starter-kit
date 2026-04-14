b698cdf3b5798d4e01ca89a793a12ed3aa04362c
Seb Kay <seb@sebkay.com>
2026-04-14 22:48:32 +0100

    Block suspended users from logging in

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -15,6 +15,7 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
+    'suspended' => 'Your account has been suspended. Please contact support if you think this is a mistake.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
